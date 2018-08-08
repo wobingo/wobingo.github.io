e2c5a35190c2f54793cda98b32fa2be3359a8416
Kyle Decker <kylebdecker@gmail.com>
2018-08-08 10:50:53 -0400

    Second styling pass, add jQuery

--- assets/items.js
+++ assets/items.js
@@ -1,14 +1,16 @@
 const items = document.getElementsByClassName('item');
-const itemWidth = 200;
+const itemSize = 200;
 
 function init() {
   Array.prototype.forEach.call(items, function(item) {
     // Place the items
     // Position between the top and bottom of the canvas minus some offset
-    var posX = Math.round(Math.random() * document.body.clientWidth) - itemWidth;
-    var posY = Math.round(Math.random() * document.body.clientHeight) - itemWidth;
-    item.style.cssText = `width: ${itemWidth}px;
-                         left: ${posX}px;
+    var offset = itemSize;
+    // Between 20 and 80%
+    var posX = (Math.random() * 60) + 20;
+    var posY = Math.round(Math.random() * document.body.clientHeight) - itemSize;
+    item.style.cssText = `width: ${itemSize}px;
+                         left: ${posX}%;
                          top: ${posY}px;`;
 
     item.onclick = function() {
@@ -22,4 +24,14 @@ function collectItem(item) {
   item.style.cssText = null;
 }
 
+// Add parallax scrolling effect
+function parallax() {
+  console.log("called");
+  window.addEventListener('scroll', function () {
+    var scrolled = window.pageYOffset;
+    console.log(scrolled);
+  });
+}
+
 init();
+parallax();
--- merch.md
+++ merch.md
@@ -1,5 +1,5 @@
 ---
-layout: page
+layout: default
 title: Merch
 permalink: /merch/
 ---
