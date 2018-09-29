73cb9167a69d9081cb74cda3c61f7d87cf9f2693
Kyle Decker <kylebdecker@gmail.com>
2018-09-29 01:58:50 -0400

    Remove inventory overlay, fix cursor flickering, animate logo

--- assets/scripts/items.js
+++ assets/scripts/items.js
@@ -1,25 +1,38 @@
 const items = document.getElementsByClassName('item');
-const itemWidth = 200;
+var inventoryCount;
+
+// TO DO
+// - Enable dragging inventory
+// - Enable minimizing inventory
+// - Add inventory descriptions
+// - Enable item collection
+// - Create an easier way to manage items and descriptions
 
 function init() {
   Array.prototype.forEach.call(items, function(item) {
     // Place the items
     // Position between the top and bottom of the canvas minus some offset
-    var posX = Math.round(Math.random() * document.body.clientWidth) - itemWidth;
-    var posY = Math.round(Math.random() * document.body.clientHeight) - itemWidth;
-    item.style.cssText = `width: ${itemWidth}px;
-                         left: ${posX}px;
+    var posX = Math.round(Math.random() * document.body.clientWidth);
+    var posY = Math.round(Math.random() * document.body.clientHeight);
+    item.style.cssText = `left: ${posX}px;
                          top: ${posY}px;`;
 
     item.onclick = function() {
       collectItem(item);
     }
   });
+
+  inventoryCount = 0;
 }
 
 function collectItem(item) {
   console.log("clicked" + item);
   item.style.cssText = null;
+  increaseInventoryCount();
+}
+
+function increaseInventoryCount() {
+  inventoryCount++;
 }
 
 init();
