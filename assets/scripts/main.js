3a15837895e1c23336bb7a658c0e35b3f90ceecc
Kyle Decker <kylebdecker@gmail.com>
2018-10-25 18:41:24 -0400

    Add Lazyload

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,17 @@
 let g = "font-size:20px; font-weight:bold;";
 console.log("%c🖌💻 Site designed and coded with 💖 by Kyle Decker: https://kyledecker.me", g);
 
+// Lazy Load images
+(function(w, d){
+  var b = d.getElementsByTagName('body')[0];
+  var s = d.createElement("script"); 
+  var v = !("IntersectionObserver" in w) ? "8.17.0" : "10.19.0";
+  s.async = true; // This includes the script as async. See the "recipes" section for more information about async loading of LazyLoad.
+  s.src = "https://cdn.jsdelivr.net/npm/vanilla-lazyload@" + v + "/dist/lazyload.min.js";
+  w.lazyLoadOptions = {/* Your options here */};
+  b.appendChild(s);
+}(window, document));
+
 $(document).ready(function () {
 
   //
